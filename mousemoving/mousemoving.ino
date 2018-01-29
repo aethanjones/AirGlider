0d268d513d0648510f80a9dc284aee21cc49b6fc
Ethan Jones <eaj9990@gmail.com>
2018-01-29 03:37:23 -0500

    Add files via upload

---
+++ mousemoving/mousemoving.ino
@@ -0,0 +1,12 @@
+#include <Mouse.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Mouse.move(2, 2);
+  delay(1000);
+}
